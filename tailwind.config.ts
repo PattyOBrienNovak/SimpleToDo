5a38f27c6e030ff8f57b7911d0d737c63e0e538d
PNovak619 <78633139+PNovak619@users.noreply.github.com>
2024-10-04 23:25:07 -0400

    first save to Git of MVP

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,11 +8,11 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        navy: '#1a1a2e',
+        'navy-light': '#16213e',
+        pink: '#e94560',
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
